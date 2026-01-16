e48c03dba28cd505ec3182e2344bf51e5166ff98
Geni Palla <palla.inbox@gmail.com>
2026-01-15 22:02:49 -0500

    fixed build error

--- src/collections/Opportunities/hooks/futureDate.ts
+++ src/collections/Opportunities/hooks/futureDate.ts
@@ -3,7 +3,7 @@ import { APIError } from 'payload'
 
 export const futureDate: FieldHook = async ({ value, operation, field, previousValue }) => {
   if ((operation === 'create') || (operation === 'update' && (previousValue == '' || previousValue == null))) {
-    var inputDate = new Date(value)
+    const inputDate = new Date(value)
     if (inputDate && inputDate < new Date()) {
       throw new APIError(`${field.label} cannot be in the past`, 400, value, true)
     }
